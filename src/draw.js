f547f49758d93e506159e85be9db422ec313bd8d
Andrew Gursky <agursky.js@gmail.com>
2016-12-14 19:51:07 -0500

    Added FAQ button and section. Fixed issue where larger than window boxes were not saving on Safari/mobile browswers.

--- src/draw.js
+++ src/draw.js
@@ -7,7 +7,7 @@ if (window.innerWidth < 768) {
     var colorContainerInit = 'none';
 } else {
     var colQuant = Math.floor((window.innerWidth - 395)/28) - 1;
-    var rowQuant = Math.floor((window.innerHeight - 58)/28) - 1;
+    var rowQuant = Math.floor((window.innerHeight - 78)/28) - 1;
     if (colQuant > 20) {
         colQuant = 20;
     }
@@ -95,6 +95,25 @@ var HelpWindow = function(props) {
     
 }
 
+var FAQContainer = function(props) {
+    return (
+        <div id="faq-container" style={props.style}>
+            <button type = 'button' className = 'xOut' onClick = {props.clickFunc}>&#10006;&#xFE0E;</button>
+            <div id='faq-sub-container'>
+                <h1>FAQ</h1>
+                <h2>How to Save your Work</h2>
+                <h3>In Firefox and Chrome</h3>
+                <p>In these browswers, when you press the save button (<img className='button-img' src='img/save.png' alt='Save button image'/>) the image file will automatically download into whatever folder you have set as your downloads folder. The file will be named 'Pixel Drawing.png'. For multiple files, a number will be added to the end of the file name to differentiate between drawings.</p>
+                <h3>In Safari, Mobile Safari, and Mobile Chrome</h3>
+                <p>In these browsers, when you press the save button, it will open the image in a new browswer window. From there you can save the image by touching the image (on mobile) or right clicking (on Desktop).</p>
+                <h3>In Mobile Firefox</h3>
+                <p>Saving is not supported on Mobile Firefox. Sorry:(</p>
+                <h3>If you have any further questions or feedback, I'd love to hear from you. I can be reached at <a href='mailto:agursky.js@gmail.com'>agursky.js@gmail.com</a></h3>
+            </div>
+        </div>
+    )
+}
+
 var AddRemoveWindow = function(props) {
     return (
         <div className='add-remove center' style={props.style}>
@@ -111,15 +130,6 @@ var AddRemoveWindow = function(props) {
     )
 }
 
-//var MobileContainer = function(props) {
-//       return (
-//        <div className='mobileContainer'>
-//                    <button type='button' className='mobile-button' onClick={props.colorClickFunc}><img src='img/mobColor.png' alt='Open Color Menu Button'/></button>
-//                </div>
-//    )
-//    
-//}
-
 var ButtonContainer = React.createClass({
     getInitialState: function() {
         return {
@@ -133,14 +143,15 @@ var ButtonContainer = React.createClass({
     render: function(props) {
         return(
             <div className = 'buttonContainer'>
-                    <button type='button' className='mobile-button' onClick={this.props.buttonFunc[0]} onMouseOver={function() {this.hoverFunc('Resize Grid')}.bind(this)}><img src='img/addRemove.svg' alt='Resize Grid Button'/></button>
-                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[1]} onMouseOver={function() {this.hoverFunc('Remove Column')}.bind(this)}><img src='img/remCol.png' alt='Remove Column Button'/></button> 
-                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[2]} onMouseOver={function() {this.hoverFunc('Remove Row')}.bind(this)}><img src='img/remRow.png' alt='Remove Row Button'/></button>
-                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[3]} onMouseOver={function() {this.hoverFunc('Add Column')}.bind(this)}><img src='img/addCol.png' alt='Add Column Button'/></button>
-                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[4]} onMouseOver={function() {this.hoverFunc('Add Row')}.bind(this)}><img src='img/addRow.png' alt='Add Row Button'/></button>
-                    <button type='button' onClick={this.props.buttonFunc[5]} onMouseOver={function() {this.hoverFunc('Start Over')}.bind(this)}><img src='img/clear.png' alt='Clear Button'/></button>
-                    <button type='button' onClick={this.props.buttonFunc[6]} onMouseOver={function() {this.hoverFunc('Save')}.bind(this)}><img src='img/save.png' alt='Save Button'/></button>
-                    <button type='button' className='mobile-button' onClick={this.props.buttonFunc[7]} onMouseOver={function() {this.hoverFunc('Choose a Color')}.bind(this)}><img src='img/mobColor.png' alt='Open Color Menu Button'/></button>
+                    <button type='button' onClick={this.props.buttonFunc[0]} onMouseOver={function() {this.hoverFunc('Help')}.bind(this)}><img src='img/help.svg' alt='Help Button'/></button> 
+                    <button type='button' className='mobile-button' onClick={this.props.buttonFunc[1]} onMouseOver={function() {this.hoverFunc('Resize Grid')}.bind(this)}><img src='img/addRemove.svg' alt='Resize Grid Button'/></button>
+                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[2]} onMouseOver={function() {this.hoverFunc('Remove Column')}.bind(this)}><img src='img/remCol.png' alt='Remove Column Button'/></button> 
+                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[3]} onMouseOver={function() {this.hoverFunc('Remove Row')}.bind(this)}><img src='img/remRow.png' alt='Remove Row Button'/></button>
+                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[4]} onMouseOver={function() {this.hoverFunc('Add Column')}.bind(this)}><img src='img/addCol.png' alt='Add Column Button'/></button>
+                    <button type='button' className='desktop-button' onClick={this.props.buttonFunc[5]} onMouseOver={function() {this.hoverFunc('Add Row')}.bind(this)}><img src='img/addRow.png' alt='Add Row Button'/></button>
+                    <button type='button' onClick={this.props.buttonFunc[6]} onMouseOver={function() {this.hoverFunc('Start Over')}.bind(this)}><img src='img/clear.png' alt='Clear Button'/></button>
+                    <button type='button' onClick={this.props.buttonFunc[7]} onMouseOver={function() {this.hoverFunc('Save')}.bind(this)}><img src='img/save.png' alt='Save Button'/></button>
+                    <button type='button' className='mobile-button' onClick={this.props.buttonFunc[8]} onMouseOver={function() {this.hoverFunc('Choose a Color')}.bind(this)}><img src='img/mobColor.png' alt='Open Color Menu Button'/></button>
                     <span className='helper-span'>{this.state.helperText}</span>
                 </div>
         )
@@ -157,7 +168,8 @@ var Application = React.createClass({
             modalStyle: {display: 'none'}, 
             colorContainerStyle: {},
             helpStyle: {}, 
-            addRemoveStyle: {}
+            addRemoveStyle: {}, 
+            faqStyle: {}
         };
     },
     changeBoxColor: function(val) {
@@ -221,14 +233,38 @@ var Application = React.createClass({
         
     },
     saveImage: function() {
-        html2canvas(document.getElementsByClassName('boxContainer'), {
-            onrendered: function(canvas) {
+        console.log('test casem2');
+//        html2canvas(document.getElementById('boxContainer'), {
+//            onrendered: function(canvas) {
+//                canvas.toBlob(function(blob) {
+//                saveAs(blob, "Pixel Drawing.png");
+//                }
+//            );
+//            }
+//        })
+        $('#boxContainer').css({
+            position: 'absolute',
+            top: 0,
+            left: 0
+        });
+        html2canvas(document.getElementById('boxContainer'), {
+            onrendered: (function(canvas) {
                 canvas.toBlob(function(blob) {
                 saveAs(blob, "Pixel Drawing.png");
-                }
-            );
-        }
-    })},
+                });
+                {
+                    $('#boxContainer').css({
+                        position: 'static',
+                        top: 'initial',
+                        left: 'initial'
+                    });
+                };
+                
+            })
+        });
+        
+    
+    },
     showModal: function() {
         this.state.modalStyle = {display: 'initial'};
         this.setState(this.state);
@@ -281,8 +317,8 @@ var Application = React.createClass({
                         }.bind(this))}
                     </div>
                 </div> 
-                <ButtonContainer buttonFunc = {[function() {this.toggler('addRemoveStyle')}.bind(this), this.removeColumn, this.removeRow, this.addColumn, this.addRow, this.showModal, this.saveImage, function() {this.toggler('colorContainerStyle')}.bind(this)]}/>
-                <div className = 'boxContainer'>
+                <ButtonContainer buttonFunc = {[function() {this.toggler('faqStyle')}.bind(this), function() {this.toggler('addRemoveStyle')}.bind(this), this.removeColumn, this.removeRow, this.addColumn, this.addRow, this.showModal, this.saveImage, function() {this.toggler('colorContainerStyle')}.bind(this)]}/>
+                <div id = 'boxContainer'>
                     <div className='table-container'>
                             {this.state.drawingTable.map(function(item, index) {
                                 return (        
@@ -292,6 +328,7 @@ var Application = React.createClass({
                 </div>
                 <Modal modalMessage={this.state.modalMessage} style={this.state.modalStyle} removeModal={this.removeModal} confirmModal={this.confirmModal}/>
                 <AddRemoveWindow style={this.state.addRemoveStyle} addRemFunc={[this.addRow, this.addColumn, this.removeRow, this.removeColumn, function() {this.toggler('addRemoveStyle');}.bind(this)]}/>
+                <FAQContainer style={this.state.faqStyle} clickFunc = {function() {this.toggler('faqStyle');}.bind(this)}/>
 
             </div>
         )
